a14dc619f58b9a7aced50573d1d069b192a0db41
RyotaOnodera124 <onoryo.0124@gmail.com>
2023-09-25 14:10:11 +0900

    a-5完了

---
+++ a-4.py
@@ -0,0 +1,6 @@
+members = ["Bob", "Tom", "Ken"]
+
+# "Bob" と "Tom" を取得して出力
+for member in members:
+    if member == "Bob" or member == "Tom":
+        print(member)
---
+++ a-5.py
@@ -0,0 +1,7 @@
+bob_info = ["Bob", "Dylan", 79]
+
+# リストから情報を取得して文字列を構築し、出力
+name = f"Name: {bob_info[0]} {bob_info[1]}"
+age = f"Age: {bob_info[2]}"
+output = f"{name}, {age}"
+print(output)
